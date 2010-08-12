b546317b1f3e1ec10c445f5329d2c92e942857de
Dan Steinicke <dansteinicke@gmail.com>
2010-08-12 09:29:58 -0700

    Add links to delete addresses

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function remove_fields(link){
+  debugger;
+  $(link).previous("input[type=hidden]").value = "1";
+  $(link).up(".fields").hide();
+}
